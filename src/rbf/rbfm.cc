98b34e94eb4587da9b519b8cb9f6d660547fe0cd
dgli <facultyofmusic@gmail.com>
2018-10-11 21:45:21 -0700

    Add stubbed implementation for RBFM
    RecordBasedFileManager has unimplemented functions that are referenced
    from tests. This commit provides stubbed implementations for
    updateRecord, readAttribute, and scan. Stubbed implementations simply
    return -1.

--- src/rbf/rbfm.cc
+++ src/rbf/rbfm.cc
@@ -45,3 +45,27 @@ RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attri
 RC RecordBasedFileManager::printRecord(const vector<Attribute> &recordDescriptor, const void *data) {
     return -1;
 }
+
+RC RecordBasedFileManager::deleteRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid) {
+	return -1;
+}
+
+// Assume the RID does not change after an update
+RC RecordBasedFileManager::updateRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, const RID &rid) {
+
+}
+
+RC RecordBasedFileManager::readAttribute(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid, const string &attributeName, void *data) {
+
+}
+
+// Scan returns an iterator to allow the caller to go through the results one by one.
+RC RecordBasedFileManager::scan(FileHandle &fileHandle,
+    const vector<Attribute> &recordDescriptor,
+    const string &conditionAttribute,
+    const CompOp compOp,                  // comparision type such as "<" and "="
+    const void *value,                    // used in the comparison
+    const vector<string> &attributeNames, // a list of projected attributes
+    RBFM_ScanIterator &rbfm_ScanIterator) {
+
+}
